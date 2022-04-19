a586e186ba7157db2cccef8cb8b8444d7f304f5d
prono69 <faltu9851@gmail.com>
2022-04-19 18:31:02 +0530

    ok

--- t-header.sh
+++ t-header.sh
@@ -36,16 +36,13 @@ COPY_FILES() {
         mkdir -p ~/.termux/;
         if [[ "$version" -le 7 ]]; then
                 rm -rf $PREFIX/share/figlet/ASCII-Shadow.flf
-                cp .object/color*.* .object/font*.* ~/.termux/
                 cp .object/ASCII-Shadow.flf $PREFIX/share/figlet/
 		cp .banner.sh ~/
 		termux-reload-settings
 
         else
                 rm -rf $PREFIX/share/figlet/ASCII-Shadow.flf
-                cp .object/color*.* .object/font*.* ~/.termux/;
                 cp .object/ASCII-Shadow.flf $PREFIX/share/figlet/
-                cp .object/termux.properties ~/.termux/
 		cp .banner.sh ~/
 		termux-reload-settings
         fi
@@ -56,24 +53,13 @@ COPY_FILES() {
 		termux-reload-settings
 	fi
 }
-rubygem_d () {
-dpkg -s ruby2 &> /dev/null
-if [[ $? -eq 0 ]]; then
-	apt install --reinstall ruby2 -y;
-	gem install lolcat*.gem &> /dev/null
-else
-	apt install --reinstall ruby -y;
-	gem install lolcat*.gem &> /dev/null
-fi
-	
-}
+
 # note this is only print 7 charecters
 echo "";
 echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";
 echo "";
 (apt update -y && apt upgrade -y) &> /dev/null;
 apt install figlet pv ncurses-utils binutils coreutils wget git zsh termux-api procps gawk exa termux-tools -y &> /dev/null;
-rubygem_d &> /dev/null
 termux-wake-lock;
 if [ -e $PREFIX/share/figlet/Remo773.flf ]; then
 	echo -e "\e[1;34m[*] \033[32mRemo773.flf figlet font is present\033[0m";
